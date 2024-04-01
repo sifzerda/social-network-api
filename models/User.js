2213d30e1e77826d7278fc6b51b79d55e3ccadcb
Troy <tydamon@hotmail.com>
2024-04-01 21:11:24 +1100

    created user model

---
+++ models/User.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            default: true,
+            required: true,
+            unique: true,
+            match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Please enter a valid email address']
+        },
+        thoughts: {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        },
+        friends: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+    },
+    {
+        toJSON: { virtuals: true, },
+        id: false,
+    });
+
+// creates virtual:
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('course', userSchema);
+
+module.exports = User;
+
+
+//////////////////////////////////////
+
+// alternate form of email validation:
+
+//email: {
+//    type: String,
+//    default: true,
+//    required: true,
+//    unique: true,
+//    validate: {
+//      validator: function(email) {
+//          //regex to match email:
+//         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+//      },
+//      message: props => `${props.value} is not a valid email address!`
+//    }
+//  },
