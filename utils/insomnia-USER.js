2ce372e65ca849f9dd64c3a9c763758f40a96039
Troy <tydamon@hotmail.com>
2024-04-05 00:44:30 +1100

    all routes working except for UPDATE a user by id

--- utils/insomnia-USER.js
+++ utils/insomnia-USER.js
@@ -1,24 +1,12 @@
-//     POST            localhost:3001/api/users       -- works
-
-//     GET            localhost:3001/api/users        -- works
-
-//                 !! id not being recognized !!
-
-//     GET id            localhost:3001/api/users/             ids
-//                                                  660d6794027bdbc11533f781
-//                                                  660d6bd2027bdbc11533f783
-//                                                  660d6bd7027bdbc11533f785
-//                                                  660d6c27027bdbc11533f789
-//                                                  660d6c37027bdbc11533f78b
-// 
-
-//     DELETE id         localhost:3001/api/users/1     -- 
-
-//     PUT id            localhost:3001/api/users/1     -- 
-
-
 
 // ALL THOUGHT ROUTES ARE WORKING 
+// USER ROUTES THAT ARE WORKING:
+// get all users;
+// get one user by id;
+// create user;
+// delete user;
+
+// ** only update not working
 
 {
     "username": "lernantino",
